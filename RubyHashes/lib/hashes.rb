50c04cbe7a74ccb0ecd5f402e6a82ba70928cc4e
Kyle Long <klong57@uncc.edu>
2020-04-07 14:43:17 +0000

    Install and Set Up Cucumber

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -16,6 +16,17 @@ end
 # Part II
 def array2d_2_hash contact_info, contacts
     # YOUR CODE HERE
+    x = 0
+    null = 0
+    if contact_info[null][null] == nil
+        return contacts
+    else
+        contacts.each do |key,value|
+            contacts[key] = {"email" => contact_info[x][0] , "phone" => contact_info[x][1]}
+            x+=1
+        end
+    end
+    return contacts
 end
 
 # Part III
