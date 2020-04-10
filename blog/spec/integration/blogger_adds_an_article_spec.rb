43fca466f3db021182dd906a8e8d03eb88f7423d
Kyle Long <klong57@uncc.edu>
2020-04-10 12:33:28 +0000

    Added scenario for editing articles

--- blog/spec/integration/blogger_adds_an_article_spec.rb
+++ blog/spec/integration/blogger_adds_an_article_spec.rb
@@ -37,3 +37,33 @@ feature"Blogger Shows an article" do
     end
 end
 
+feature"Blogger Edit an article" do
+    scenario "Blogger successfully navigates to the edit articles page from the listing articles page" do
+        visit new_article_path
+        expect(page).to have_content("New Article")
+        fill_in "Title", with: "New Capybara Article"
+        fill_in "Text", with: "This is a new capybara article"
+        click_button "Create Article"
+        expect(page). to have_content("New Capybara Article")
+        expect(page). to have_content("This is a new capybara article")
+        visit articles_path
+        click_link "Edit"
+        expect(page).to have_field("Title")
+        expect(page). to have_field("Text")
+    end
+    
+    scenario "Blogger successfully edits a new article" do
+        visit new_article_path
+        expect(page).to have_content("New Article")
+        fill_in "Title", with: "New Capybara Article"
+        fill_in "Text", with: "This is a new capybara article"
+        click_button "Create Article"
+        visit articles_path
+        click_link "Edit"
+        fill_in "Text", with: ""
+        fill_in "Text", with: "This is the updated capybara article"
+        click_button "Update Article"
+        expect(page). to have_content("New Capybara Article")
+        expect(page). to have_content("This is the updated capybara article")
+    end
+end
